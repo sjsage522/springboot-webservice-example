4152e0b84541149c12005329e6f1fd507739ffac
shinjunseok <sjsage522@gmail.com>
2021-08-07 18:35:19 +0900

    Initial commit

---
+++ src/main/java/io/wisoft/springbootwebserviceexample/SpringbootWebserviceExampleApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.springbootwebserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebserviceExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wisoft/springbootwebserviceexample/SpringbootWebserviceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wisoft.springbootwebserviceexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
