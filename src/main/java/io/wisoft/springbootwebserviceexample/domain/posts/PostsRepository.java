84efeda0f3e4ae29923b836ca08b3e6ca0900004
shinjunseok <sjsage522@gmail.com>
2021-08-08 19:04:43 +0900

    update : oauth 설정 파일 등록까지 구현
    - 게시글 관련 CRUD API 구현
    - 뷰 레이어 구현

--- src/main/java/io/wisoft/springbootwebserviceexample/domain/posts/PostsRepository.java
+++ src/main/java/io/wisoft/springbootwebserviceexample/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package io.wisoft.springbootwebserviceexample.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/io/wisoft/springbootwebserviceexample/service/posts/PostsService.java
+++ src/main/java/io/wisoft/springbootwebserviceexample/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package io.wisoft.springbootwebserviceexample.service.posts;
 
 import io.wisoft.springbootwebserviceexample.domain.posts.Posts;
 import io.wisoft.springbootwebserviceexample.domain.posts.PostsRepository;
+import io.wisoft.springbootwebserviceexample.web.dto.PostsListResponseDto;
 import io.wisoft.springbootwebserviceexample.web.dto.PostsResponseDto;
 import io.wisoft.springbootwebserviceexample.web.dto.PostsSaveRequestDto;
 import io.wisoft.springbootwebserviceexample.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,10 +34,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
